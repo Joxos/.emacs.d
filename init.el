afd0e3d12eeecc80e9f0d7db0848b865bd1a73ee
Joxos <xujunhao61@163.com>
2022-02-13 17:01:51 +0800

    Little update.

--- init.el
+++ init.el
@@ -22,18 +22,18 @@
 ;;       #'command-completion-default-include-p)
 
 ;; Do not allow the cursor in the minibuffer prompt
-(setq minibuffer-prompt-properties
-      '(read-only t cursor-intangible t face minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+;; (setq minibuffer-prompt-properties
+;;       '(read-only t cursor-intangible t face minibuffer-prompt))
+;; (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
 ;; Add prompt indicator to `completing-read-multiple'.
 ;; Alternatively try `consult-completing-read-multiple'.
-(defun crm-indicator (args)
-  (cons (concat "[CRM] " (car args)) (cdr args)))
-(advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+;; (defun crm-indicator (args)
+;;   (cons (concat "[CRM] " (car args)) (cdr args)))
+;; (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
 
 ;; Enable recursive minibuffers
-(setq enable-recursive-minibuffers t)
+;; (setq enable-recursive-minibuffers t)
 
 ;; ESC to quit
 (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
@@ -249,8 +249,7 @@
   ;; (corfu-scroll-margin 5)        ;; Use scroll margin
 
   ;; You may want to enable Corfu only for certain modes.
-  :hook ((prog-mode . corfu-mode)
-         (shell-mode . corfu-mode)
+  :hook (((python-mode c++-mode shell-mode) . corfu-mode)
          (eshell-mode . (lambda ()
 			  (setq-local corfu-quit-at-boundary t
 				      corfu-quit-no-match t
@@ -306,6 +305,66 @@
   :straight t
   :hook ((prog-mode org-mode) . #'yas-minor-mode)
   :config
+  ;; (defun check-expansion ()
+  ;;   (save-excursion
+  ;;     (if (looking-at "\\_>") t
+  ;; 	(backward-char 1)
+  ;; 	(if (looking-at "\\.") t
+  ;; 	  (backward-char 1)
+  ;; 	  (if (looking-at "->") t nil)))))
+  ;; (defun do-yas-expand ()
+  ;;   (let ((yas/fallback-behavior 'return-nil))
+  ;;     (yas/expand)))
+  ;; (defun tab-indent-or-complete ()
+  ;;   (interactive)
+  ;;   (cond
+  ;;    ((minibufferp)
+  ;;     (minibuffer-complete))
+  ;;    (t
+  ;;     (indent-for-tab-command)
+  ;;     (if (or (not yas/minor-mode)
+  ;;             (null (do-yas-expand)))
+  ;; 	  (if (check-expansion)
+  ;;             (progn
+  ;; 		(company-manual-begin)
+  ;; 		(if (null company-candidates)
+  ;; 		    (progn
+  ;; 		      (company-abort)
+  ;; 		      (indent-for-tab-command)))))))))
+  ;; (defun tab-complete-or-next-field ()
+  ;;   (interactive)
+  ;;   (if (or (not yas/minor-mode)
+  ;; 	    (null (do-yas-expand)))
+  ;; 	(if company-candidates
+  ;; 	    (company-complete-selection)
+  ;; 	  (if (check-expansion)
+  ;; 	      (progn
+  ;; 		(company-manual-begin)
+  ;; 		(if (null company-candidates)
+  ;; 		    (progn
+  ;; 		      (company-abort)
+  ;; 		      (yas-next-field))))
+  ;; 	    (yas-next-field)))))
+  ;; (defun expand-snippet-or-complete-selection ()
+  ;;   (interactive)
+  ;;   (if (or (not yas/minor-mode)
+  ;; 	    (null (do-yas-expand))
+  ;; 	    (company-abort))
+  ;; 	(company-complete-selection)))
+  ;; (defun abort-company-or-yas ()
+  ;;   (interactive)
+  ;;   (if (null company-candidates)
+  ;; 	(yas-abort-snippet)
+  ;;     (company-abort)))
+  ;; (global-set-key [tab] 'tab-indent-or-complete)
+  ;; (global-set-key (kbd "TAB") 'tab-indent-or-complete)
+  ;; (global-set-key [(control return)] 'company-complete-common)
+  ;; (define-key company-active-map [tab] 'expand-snippet-or-complete-selection)
+  ;; (define-key company-active-map (kbd "TAB") 'expand-snippet-or-complete-selection)
+  (define-key yas-keymap [tab] nil)
+  (define-key yas-keymap (kbd "C-n") 'yas-next-field)
+  (define-key yas-keymap (kbd "C-p") 'yas-prev-field)
+
   (yas-reload-all))
 ;; (use-package yasnippet-snippets
 ;;   :straight t
