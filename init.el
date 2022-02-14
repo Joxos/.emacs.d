227b251ebbd30a96b2c1b42b0b4e91b520ec0ad5
Joxos <xujunhao61@163.com>
2022-02-14 13:19:38 +0800

    Update

--- init.el
+++ init.el
@@ -35,14 +35,6 @@
 ;; Enable recursive minibuffers
 ;; (setq enable-recursive-minibuffers t)
 
-;; ESC to quit
-(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
-(define-key key-translation-map [escape] (kbd "C-g"))
-
-;; remap keys searching history in minibuffer
-;; (define-key minibuffer-local-map (kbd "C-n") 'next-history-element)
-;; (define-key minibuffer-local-map (kbd "C-p") 'previous-history-element)
-
 ;; show buffer name as window title
 ;; (setq frame-title-format "%b")
 
@@ -605,7 +597,6 @@
     "i" 'indent-region
     "o" 'other-window
     "s" 'consult-line
-    "t" 'treemacs-select-window
     "g" 'magit
     ";" 'comment-or-uncomment
     "=" 'balance-windows
@@ -655,130 +646,18 @@
 (use-package ace-window
   :straight t
   :bind ("C-x o" . 'ace-window))
-(use-package treemacs
-  :straight t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+(use-package dired
   :config
-  (progn
-    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-event-delay                5000
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-follow-delay               0.2
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-indentation                     2
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           nil
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   nil
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
-          treemacs-show-cursor                     nil
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      nil
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           35
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode 'always)
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
-
-    (treemacs-hide-gitignored-files-mode nil))
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t d"   . treemacs-select-directory)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-(use-package treemacs-evil
-  :straight t
-  :after (treemacs evil))
-(use-package lsp-treemacs
-  :straight t
-  :init
-  (lsp-treemacs-sync-mode 1))
-;; (use-package projectile
-;;   :straight t
-;;   :init
-;;   (projectile-mode +1)
-;;   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
-;; (use-package treemacs-projectile
-;;   :straight t
-;;   :after (treemacs projectile))
-;; (use-package treemacs-icons-dired
-;;   :hook (dired-mode . treemacs-icons-dired-enable-once)
-;;   :ensure t)
-;; (use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
-;;   :after (treemacs persp-mode) ;;or perspective vs. persp-mode
-;;   :ensure t
-;;   :config (treemacs-set-scope-type 'Perspectives))
-(use-package treemacs-magit
-  :straight t
-  :after (treemacs magit))
+  (setq delete-by-moving-to-trash t)
+  ;; (setq dired-dwim-target t)
+  ;; (setq dired-listing-switches
+  ;;       "-AGhlv --group-directories-first --time-style=long-iso")
+  )
+(use-package dirvish
+  :straight t)
 
 ;; RESET GC
 (setq gc-cons-threshold original-gc-cons-threshold)
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("729ddf899d07810d66fb6bd048b1cbef228efbcee0dca69d3d6cd0efcff428e1" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
