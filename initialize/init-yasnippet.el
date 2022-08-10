e097afc98455899fce7e841cb8e2ef93c7b9ae83
Joxos <johndoe@example.com>
2022-08-10 15:35:21 +0800

    fix yasnippet

--- initialize/init-yasnippet.el
+++ initialize/init-yasnippet.el
@@ -1,10 +1,13 @@
 (use-package yasnippet
   :straight t
+  ;; note that this is needed since yasnippet automatically choose the region
+  :hook (after-init-hook . delete-selection-mode)
   :config
   (yas-global-mode 1)
-  ;; (define-key yas-keymap [tab] nil)
-  ;; (define-key yas-keymap (kbd "C-n") 'yas-next-field)
-  ;; (define-key yas-keymap (kbd "C-p") 'yas-prev-field)
+  (define-key yas-keymap (kbd "TAB") nil)
+  (define-key yas-keymap (kbd "<tab>") nil)
+  (define-key yas-keymap (kbd "C-n") 'yas-next-field-or-maybe-expand)
+  (define-key yas-keymap (kbd "C-p") 'yas-prev-field)
   (yas-reload-all))
 
 (provide 'init-yasnippet)
