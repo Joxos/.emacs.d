a440b5452a8d56f5ffdaadcc3d0a7f3b2079510e
Joxos <johndoe@example.com>
2022-09-02 15:00:00 +0800

    update

--- initialize/init-web-mode.el
+++ initialize/init-web-mode.el
@@ -1,14 +1,28 @@
+;; web mode
 (use-package web-mode
   :straight t
+  :mode "\\.\\(phtml\\|php\\|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
   :config
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode)))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+;; CSS mode
+(use-package css-mode
+  :straight nil
+  :init (setq css-indent-offset 2))
+
+;; SCSS mode
+(use-package scss-mode
+  :straight t
+  :init
+  ;; disable complilation on save
+  (setq scss-compile-at-save nil))
+
+;; new `less-css-mde' in Emacs 26
+(use-package less-css-mode :straight t)
+
+;; JSON mode
+(use-package json-mode :straight t)
 
 (provide 'init-web-mode)
