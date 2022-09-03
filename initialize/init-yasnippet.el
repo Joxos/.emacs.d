22cc17d0c1430861282aa8cf6a05ec0d60d0c037
Joxos <johndoe@example.com>
2022-09-03 12:57:51 +0800

    update yas

--- initialize/init-yasnippet.el
+++ initialize/init-yasnippet.el
@@ -4,10 +4,9 @@
   :hook (after-init-hook . delete-selection-mode)
   :config
   (yas-global-mode 1)
-  (define-key yas-keymap (kbd "TAB") nil)
-  (define-key yas-keymap (kbd "<tab>") nil)
-  (define-key yas-keymap (kbd "C-n") 'yas-next-field-or-maybe-expand)
-  (define-key yas-keymap (kbd "C-p") 'yas-prev-field)
-  (yas-reload-all))
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "C-n") 'yas-next-field-or-maybe-expand)
+  (define-key yas-minor-mode-map (kbd "C-p") 'yas-prev-field))
 
 (provide 'init-yasnippet)
