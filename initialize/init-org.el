369ad6fc051ba22fb2fb827f02ce3bb20eb38161
Joxos <johndoe@example.com>
2022-09-03 17:21:03 +0800

    update

--- initialize/init-org.el
+++ initialize/init-org.el
@@ -1,14 +1,6 @@
-(use-package org-bars
-  :straight (:host github :repo "tonyaldon/org-bars")
-  :init
-  (setq org-bars-stars '(:empty "*"
-				:invisible "+"
-				:visible "-"))
+(use-package org-bullets
+  :straight t
   :config
-  (add-hook 'org-mode-hook #'org-bars-mode)
-  :custom-face
-  (org-level-1 ((t (:height 1.5))))
-  (org-level-2 ((t (:height 1.3))))
-  (org-level-3 ((t (:height 1.1))))
-  (org-level-4 ((t (:height 1.05)))))
+  (add-hook 'org-mode-hook 'org-bullets-mode))
+
 (provide 'init-org)
