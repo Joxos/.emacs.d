ddf3b450f8f30a0ec8b34e47780ef85c919ade65
Joxos <johndoe@example.com>
2023-07-14 11:03:42 +0800

    update

--- initialize/init-lsp-bridge.el
+++ initialize/init-lsp-bridge.el
@@ -1,5 +1,3 @@
-(use-package posframe :straight t)
-(require 'init-yasnippet)
 (use-package markdown-mode
   :straight t
   :mode ("README\\.md\\'" . gfm-mode)
@@ -7,10 +5,11 @@
 ;; (straight-use-package
 ;;  '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"))
 (add-to-list 'load-path "~/.emacs.d/lsp-bridge/")
+(require 'init-yasnippet)
 (require 'lsp-bridge)
 (setq lsp-bridge-python-lsp-server "jedi")
 (setq acm-backend-lsp-enable-auto-import nil)
-;; (append lsp-bridge-multi-lang-server-extension-list '(("html") . "html_emmet"))
+(append lsp-bridge-multi-lang-server-extension-list '(("html") . "html_emmet"))
 (setq lsp-bridge-multi-lang-server-extension-list '((("vue") . "volar_emmet")
 						    (("html") . "html_emmet")
 						    (("css") . "css_emmet")))
